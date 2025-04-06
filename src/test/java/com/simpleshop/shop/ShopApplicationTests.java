ba1f21d4ab9df58dcd5d0edabe1ce34289b6b1cf
minho <minho2618@naver.com>
2025-04-07 00:04:03 +0900

    Init

---
+++ src/test/java/com/simpleshop/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleshop.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
